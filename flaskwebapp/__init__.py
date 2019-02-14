a35e27349cbce5a06c5e6e3094be09fb56b1e867
Zsolt Leitner <dxtrlbrtry@yahoo.com>
2019-02-14 15:58:33 +0200

    implemented pagination for posts implemented reset password with mailing service

--- flaskwebapp/__init__.py
+++ flaskwebapp/__init__.py
@@ -1,7 +1,9 @@
+import os
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask import Flask
 from flask_login import LoginManager
+from flask_mail import Mail
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'd0ec579573c2f24c906a6d790c1d9b82'
@@ -12,5 +14,11 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = 'flaskzleitner@gmail.com'
+app.config['MAIL_PASSWORD'] = '#Arobs123'
+mail = Mail(app)
 
 from flaskwebapp import routes
